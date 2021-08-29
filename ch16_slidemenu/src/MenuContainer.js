d19edbe342bd5736fe28f9fc24049e921e734d0d
karyshev_m <karyshev63rus@gmail.com>
2021-08-29 08:41:16 +0400

    added ch16_slidemenu

---
+++ ch16_slidemenu/src/MenuContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import MenuButton from './MenuButton'
+import Menu from './Menu'
+
+class MenuContainer extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            visible: false
+        }
+        this.handleMouseDown = this.handleMouseDown.bind(this)
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
+
+    handleMouseDown (e) {
+        this.toggleMenu()
+        console.log('clicked')
+        e.stopPropagation()
+    }
+
+    toggleMenu () {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    render () {
+        return (
+            <div>
+            <MenuButton handleMouseDown={this.handleMouseDown} />
+            <Menu handleMouseDown={this.handleMouseDown}
+                    menuVisibility={this.state.visible} />
+                <div>
+                    <p>Найдешь пункт, который здесь лишний?</p>
+                    <ul>
+                    <li>Alpha</li>
+                    <li>Bravo</li>
+                    <li>Charlie</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MenuContainer

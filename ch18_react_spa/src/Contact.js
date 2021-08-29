482486f853e62d86eac7d1740cfabf11f8625b77
karyshev_m <karyshev63rus@gmail.com>
2021-08-29 11:45:23 +0400

    added ch18_react_spa

---
+++ ch18_react_spa/src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render () {
+        return (
+            <div>
+                <h2>ЕСТЬ ВОПРОСЫ? </h2>
+                <p>
+                    Опубликуйте свой вопрос на <a href='https://docent63.ru'> форуме</a>.
+                </p>
+            </div>
+        )        
+    }
+}
+
+export default Contact
---
+++ ch18_react_spa/src/Stuff.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Stuff extends Component {
+    render () {
+        return (
+            <div>
+                <h2>Продукты</h2>
+                <p>Mauris sem velit, vehicula eget sodales vitae,
+                    rhoncus eget sapien:</p>
+                <ol>
+                <li>Aphla</li>
+                <li>Bravo</li>
+                <li>Carlie</li>
+                <li>Dump</li>
+                <li>Error</li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default Stuff

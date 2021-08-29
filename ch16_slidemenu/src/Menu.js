5c02406a723643a4f546cbc549358b10020c3434
karyshev_m <karyshev63rus@gmail.com>
2021-08-29 10:08:12 +0400

    modified ch16_slidenemu about PureComponent

--- ch16_slidemenu/src/Menu.js
+++ ch16_slidemenu/src/Menu.js
@@ -8,15 +8,15 @@ class Menu extends Component {
         if (this.props.menuVisibility) {
             visibility = 'show'
         }
-    
+        console.log('Rendering: Menu')
         return (
             <div id='flyoutMenu'
                  onMouseDown={this.props.handleMouseDown}
                  className={visibility}>
-                <h2><a href='#'>Главная</a></h2>
-                <h2><a href='#'>About</a></h2>
-                <h2><a href='#'>Contact</a></h2>
-                <h2><a href='#'>Search</a></h2>
+                <h2><a href='https://docent63.ru'>Главная</a></h2>
+                <h2><a href='https://docent63.ru'>About</a></h2>
+                <h2><a href='https://docent63.ru'>Contact</a></h2>
+                <h2><a href='https://docent63.ru'>Search</a></h2>
             </div>
         )
     }
--- ch16_slidemenu/src/MenuContainer.js
+++ ch16_slidemenu/src/MenuContainer.js
@@ -25,6 +25,7 @@ class MenuContainer extends Component {
     }
 
     render () {
+        console.log('Rendering: MenuContainer')
         return (
             <div>
             <MenuButton handleMouseDown={this.handleMouseDown} />
